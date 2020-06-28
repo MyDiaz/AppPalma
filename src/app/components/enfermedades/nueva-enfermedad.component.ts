a5498f43026aebce5ac4dc4366a0c36f0c637e6a
MyDiaz <diazmaya947@ymail.com>
2020-06-27 22:38:16 -0500

    Formulario enfermedades
    Construcción del formulario y asignación de validaciones

--- src/app/components/enfermedades/nueva-enfermedad.component.ts
+++ src/app/components/enfermedades/nueva-enfermedad.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl,FormBuilder, Validators } from '@angular/forms';
+import { FormGroup, FormControl,FormBuilder, Validators, FormArray } from '@angular/forms';
 
 @Component({
   selector: 'app-nueva-enfermedad',
@@ -9,36 +9,46 @@ import { FormGroup, FormControl,FormBuilder, Validators } from '@angular/forms';
 export class NuevaEnfermedadComponent implements OnInit {
   
   NuevaEnfermedadForm: FormGroup;
-
+  etapas:string[] = [];
+  etapa:string;
   bandera:boolean = false;
 
   constructor(private fb: FormBuilder) { 
     this.NuevaEnfermedadForm = new FormGroup({
-      nombre_enfermedad: new FormControl(),
-      etapa_enfermedad : new FormControl()
+      nombre_enfermedad : new FormControl(),
+      etapas_enfermedad : this.fb.array([])
     });
-
+    this.crearFormulario();
   }
 
-  nombreEnfermedadNoValido() {
-    return this.NuevaEnfermedadForm.get('nombre_enfermedad').invalid && this.NuevaEnfermedadForm.get('nombre_enfermedad').touched;s
+  get nombreEnfermedadNoValido() {
+    return this.NuevaEnfermedadForm.get('nombre_enfermedad').invalid && this.NuevaEnfermedadForm.get('nombre_enfermedad').touched;
   }
+  
+  get etapasEnfermedad() {
+    return this.NuevaEnfermedadForm.get('etapas_enfermedad') as FormArray;
+  }
+
   ngOnInit() {
   }
   
   crearFormulario(){
     this.NuevaEnfermedadForm= this.fb.group({
-      nombre_enfermedad: ['',[ Validators.required, Validators.minLength(3)] ]
+      nombre_enfermedad: ['', [Validators.required, Validators.minLength(3)] ],
+      etapas_enfermedad: this.fb.array([])
+      
     });
   }
   
+  addEtapa () {
+      this.etapasEnfermedad.push( this.fb.control('') );
+  }
 
-
-
-
-
-
+guardar(){
+  console.log("valor", this.NuevaEnfermedadForm.value);
+  console.log("estado", this.NuevaEnfermedadForm.status);
+}
   
-
+  
   
 }
