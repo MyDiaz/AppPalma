01173299fdff1b1fc234c10299b46c0e3334fc40
MyDiaz <diazmaya947@ymail.com>
2020-08-14 22:41:08 -0500

    agregar enfermedad funcionando
    pero aún faltan detalles

--- src/app/components/enfermedades/listado-enfermedades.component.ts
+++ src/app/components/enfermedades/listado-enfermedades.component.ts
@@ -1,41 +1,56 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { EnfermedadesService } from '../../Servicios/enfermedades.service';
 import { FormGroup, FormControl } from '@angular/forms';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-listado-enfermedades',
   templateUrl: './listado-enfermedades.component.html',
   styles: []
 })
-export class ListadoEnfermedadesComponent implements OnInit {
+export class ListadoEnfermedadesComponent {
 
-  enfermedad:any = [];
+  enfermedadesEtapas:any = [];
+  enfermedades:any = [];
   NombreEnfermedadForm:FormGroup;
   NombreEnfermedadEditar:string;
   bandera:boolean = false;
   mensaje_error:string;
+  
+  constructor(private EnfermedadesService:EnfermedadesService, private router:Router,
+    private activatedRoute:ActivatedRoute) {
+    
+    this.EnfermedadesService.getEnfermedadesEtapas().subscribe( data => {
+      this.enfermedadesEtapas = data;
+    }, err => {
+      this.bandera = true;
+      this.mensaje_error = err.error.mensaje;
+      console.log(err);
+      if( err.status == 0 ) this.mensaje_error = "Servicio no disponible"
+    })    
 
-  constructor(private EnfermedadesService:EnfermedadesService) {
     this.EnfermedadesService.getEnfermedades().subscribe( data => {
-      this.enfermedad = data;
+      this.enfermedades = data;
     }, err => {
       this.bandera = true;
       this.mensaje_error = err.error.mensaje;
       console.log(err);
       if( err.status == 0 ) this.mensaje_error = "Servicio no disponible"
     })
+
     this.NombreEnfermedadForm = new FormGroup({
-      nombre_enfermedad: new FormControl()
+      id_enfermedad: new FormControl()
     }) 
     
    }
 
-  ngOnInit() {
+  editarEnfermedad(){
+    console.log(this.NombreEnfermedadForm.value.id_enfermedad.toString());
+    this.router.navigate(['editar-enfermedad',this.NombreEnfermedadForm.value.id_enfermedad.toString()], { relativeTo: this.activatedRoute });
   }
 
   enviarEnfermedad(){
-
-    console.log(this.NombreEnfermedadForm);
+    console.log(this.NombreEnfermedadForm.value.id_enfermedad);
   }
 
 }
