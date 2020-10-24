60883d4903c329034c1ed33a610ecc454cc030f3
MyDiaz <diazmaya947@ymail.com>
2020-10-24 18:34:33 -0500

    Arreglo de tabla etapas-enfermedades

--- src/app/components/enfermedades/listado-enfermedades.component.ts
+++ src/app/components/enfermedades/listado-enfermedades.component.ts
@@ -9,10 +9,15 @@ import { Router, ActivatedRoute } from '@angular/router';
   styles: []
 })
 export class ListadoEnfermedadesComponent {
+  
+  NombreEnfermedadForm:FormGroup;
+  EtapaEnfermedadForm:FormGroup;
 
   enfermedadesEtapas:any = [];
+  unique_enfermedades:any = [];
+  contador_etapas_enfermedad:any = [2,2,1,1];
   enfermedades:any = [];
-  NombreEnfermedadForm:FormGroup;
+  
   NombreEnfermedadEditar:string;
   bandera:boolean = false;
   mensaje_error:string;
@@ -21,7 +26,8 @@ export class ListadoEnfermedadesComponent {
     private activatedRoute:ActivatedRoute) {
     
     this.EnfermedadesService.getEnfermedadesEtapas().subscribe( data => {
-      this.enfermedadesEtapas = data;
+      this.enfermedadesEtapas = this.groupBy(data,"nombre_enfermedad");
+      console.log("hola", this.enfermedadesEtapas);
     }, err => {
       this.bandera = true;
       this.mensaje_error = err.error.mensaje;
@@ -31,6 +37,7 @@ export class ListadoEnfermedadesComponent {
 
     this.EnfermedadesService.getEnfermedades().subscribe( data => {
       this.enfermedades = data;
+
     }, err => {
       this.bandera = true;
       this.mensaje_error = err.error.mensaje;
@@ -39,18 +46,70 @@ export class ListadoEnfermedadesComponent {
     })
 
     this.NombreEnfermedadForm = new FormGroup({
-      id_enfermedad: new FormControl()
-    }) 
+      nombre_enfermedad: new FormControl()
+    })
     
-   }
-
-  editarEnfermedad(){
-    console.log(this.NombreEnfermedadForm.value.id_enfermedad.toString());
-    this.router.navigate(['editar-enfermedad',this.NombreEnfermedadForm.value.id_enfermedad.toString()], { relativeTo: this.activatedRoute });
+    this.EtapaEnfermedadForm = new FormGroup({
+      id_enfermedad: new FormControl()
+    })
+    //this.mostrarEnfermedadesEtapas();
   }
+  
+
+  onlyUnique(value, index, self) {
+    return self.indexOf(value) === index;
+  }
+
+  /*mostrarEnfermedadesEtapas(){
+    console.log("enfermedadesEtapas", this.enfermedadesEtapas);
+    for(let i = 0; i<this.enfermedadesEtapas.length; i++){
+      this.unique_enfermedades.push(this.enfermedadesEtapas[i].nombre_enfermedad);
+    }
+    var unique = this.unique_enfermedades.filter(this.onlyUnique);
+
+    for(let i = 0; i<unique.length; i++){
+    //[pudricion cogollo,pudricion,enfermedades]
+        let contador = 0;
+        for(let j = 0; j<this.unique_enfermedades.length; j++){
+    //[pudricion cogollo,pudricion cogollo,pudricion,pudricion,enfermedades,enfermedades]
+            if( unique[i] === this.unique_enfermedades[j] ){
+                    contador = contador+1;    
+            }
+        }
+        this.contador_etapas_enfermedad.push(contador);
+    }
+    console.log("contador_etapas_enfermedad", this.contador_etapas_enfermedad);
+    //return this.unique_enfermedades, this.contador_etapas_enfermedad; 
+    //[pudricion cogollo,pudricion,enfermedades,beto], [2,2,1,1]
+  }*/
 
   enviarEnfermedad(){
-    console.log(this.NombreEnfermedadForm.value.id_enfermedad);
+    console.log("this.NombreEnfermedadForm.value", this.NombreEnfermedadForm.value);
+    console.log("EtapaEnfermedadForm", this.EtapaEnfermedadForm.value);
   }
 
+  editarEnfermedad(){
+    if(this.NombreEnfermedadForm.value.nombre_enfermedad.startsWith("enfermedad-")){
+      this.router.navigate(['editar-enfermedad',this.NombreEnfermedadForm.value.nombre_enfermedad.replace("enfermedad-", "")]);
+    }
+    else{
+      this.router.navigate(['editar-etapa-enfermedad',this.NombreEnfermedadForm.value.nombre_enfermedad.replace("etapa-", "")]);
+    }
+  }
+
+  private groupBy(collection, property){
+    var i = 0, val, index,
+        values = [], result = [];
+    for (; i < collection.length; i++) {
+        val = collection[i][property];
+        index = values.indexOf(val);
+        if (index > -1)
+            result[index].push(collection[i]);
+        else {
+            values.push(val);
+            result.push([collection[i]]);
+        }
+    }
+    return result;
+}
 }
