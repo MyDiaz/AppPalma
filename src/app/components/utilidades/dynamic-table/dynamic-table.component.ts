47774373a82eaf138b3f56afd24ee91ad2e69cf7
MyDiaz <diazmaya947@ymail.com>
2022-11-10 11:41:36 -0500

    inicio y construcción del componente de tabla dinámica

---
+++ src/app/components/utilidades/dynamic-table/dynamic-table.component.ts
@@ -0,0 +1,43 @@
+import { SelectionModel } from '@angular/cdk/collections';
+import { Component, EventEmitter, Input, OnInit, Output, ViewChild, AfterViewInit  } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
+import { TableColumn } from '../../../models/columnsDynamicTable';
+
+@Component({
+  selector: 'app-dynamic-table',
+  templateUrl: './dynamic-table.component.html',
+  styleUrls: ['./dynamic-table.component.css']
+})
+export class DynamicTableComponent implements OnInit, AfterViewInit  {
+   
+    @Input() columns: Array<TableColumn>; //objeto conformado por el nombre de las columnas  y el nombre del campo en la base de datos
+    @ViewChild(MatPaginator, { static: false }) paginator: MatPaginator;
+    @Input() dataSource: MatTableDataSource<any>; //listado de lo que sea (cosechas, viajes...) filtrado 
+    displayedColumns: string[] = []; //nombre de las columnas obtenido a partir de columns
+    @Input() mostrarPaginador:boolean;
+    ////@Input() mostrarTablaDetalle:boolean;
+
+  
+    constructor() { }
+  
+    ngOnInit(): void {
+      console.log("this.dataset", this.dataSource.data)
+      console.log("www", this.columns.map(x => x))
+      this.displayedColumns = this.columns.map(x => x.columnDef);    // pre-fix static
+
+    }
+  
+    updateData(dataset: Array<any>){
+      this.dataSource = new MatTableDataSource<any>(dataset);
+    }
+  
+    ngAfterViewInit() {
+      this.dataSource.paginator = this.paginator;
+    }
+    
+  
+}
+  
+
+  
