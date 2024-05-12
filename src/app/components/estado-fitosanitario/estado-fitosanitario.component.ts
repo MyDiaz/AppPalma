d34dbc4df881d5a9fa0180c9621a6de65e7526f4
Mayra Riascos Diaz <diazmaya947@ymail.com>
2024-05-12 02:04:17 -0300

    Generar PDf de Estado Fitosanitario

--- src/app/components/estado-fitosanitario/estado-fitosanitario.component.ts
+++ src/app/components/estado-fitosanitario/estado-fitosanitario.component.ts
@@ -11,6 +11,7 @@ import { RegistroEnfermedad } from "src/app/models/registroEnfermedad";
 import { LoteModel } from "src/app/models/lote.models";
 import { PalmaModel } from "src/app/models/palma.model";
 //import { Router } from '@angular/router';
+import { jsPDF } from "jspdf";
 
 interface GraficoArrayMap {
   [key: string]: any[]; // Index signature with string keys and array values
@@ -54,40 +55,41 @@ export class EstadoFitosanitarioComponent implements OnInit {
   ngOnInit() {
     this.activatedRoute.queryParamMap.subscribe((params) => {
       this.nombreLoteParams = params.get("lote");
+      this._loteService.getLote(this.nombreLoteParams).subscribe(
+        (lote: LoteModel) => {
+          this.lote = lote;
+          this._loteService.getPalmasLote(this.nombreLoteParams).subscribe(
+            (palmas: PalmaModel[]) => {
+              console.log("palmas.length", palmas.length);
+              this.totalentratamiento = palmas.filter(
+                (p) => p.estado_palma === "En tratamiento"
+              ).length;
+              this.totalpendientesportratar = palmas.filter(
+                (p) => p.estado_palma === "Pendiente por tratar"
+              ).length;
+              this.totalpendientesporerradicar = palmas.filter(
+                (p) => p.estado_palma === "Pendiente por erradicar"
+              ).length;
+              this.totalerradicadas = palmas.filter(
+                (p) => p.estado_palma === "Erradicada"
+              ).length;
+              this.totalpalmas = this.lote.numero_palmas - this.totalerradicadas;
+              this.totalsanas = this.totalpalmas - (this.totalentratamiento + this.totalpendientesportratar + this.totalpendientesporerradicar + this.totalerradicadas);
+              this.incidenciareal = parseFloat((100*(this.totalentratamiento + this.totalpendientesportratar)/this.totalpalmas).toFixed(2));
+              this.casosacumulados = palmas.length;
+              this.incidenciaacumulada = parseFloat((100*(this.casosacumulados)/this.totalpalmas).toFixed(2));
+            },
+            (error) => {
+              console.error(error);
+            }
+          );
+        },
+        (error) => {
+          console.error(error);
+        }
+      );
     });
-    this._loteService.getLote(this.nombreLoteParams).subscribe(
-      (lote: LoteModel) => {
-        this.lote = lote;
-      },
-      (error) => {
-        console.error(error);
-      }
-    );
-    this._loteService.getPalmasLote(this.nombreLoteParams).subscribe(
-      (palmas: PalmaModel[]) => {
-        console.log("palmas.length", palmas.length);
-        this.totalentratamiento = palmas.filter(
-          (p) => p.estado_palma === "En tratamiento"
-        ).length;
-        this.totalpendientesportratar = palmas.filter(
-          (p) => p.estado_palma === "Pendiente por tratar"
-        ).length;
-        this.totalpendientesporerradicar = palmas.filter(
-          (p) => p.estado_palma === "Pendiente por erradicar"
-        ).length;
-        this.totalerradicadas = palmas.filter(
-          (p) => p.estado_palma === "Erradicada"
-        ).length;
-        this.totalpalmas = this.lote.numero_palmas - this.totalerradicadas;
-        this.totalsanas = this.totalpalmas - (this.totalentratamiento + this.totalpendientesportratar + this.totalpendientesporerradicar + this.totalerradicadas);
-        this.incidenciareal = parseFloat((100*(this.totalentratamiento + this.totalpendientesportratar)/this.totalpalmas).toFixed(2));
-        this.casosacumulados = palmas.length;
-        this.incidenciaacumulada = parseFloat((100*(this.casosacumulados)/this.totalpalmas).toFixed(2));
-      },
-      (error) => {
-        console.error(error);
-      }
-    );
+    
     this._loteService.getEnfermedadesServer().subscribe(
       (aux: EnfermedadNombre[]) => {
         this.enfermedades = aux;
@@ -256,5 +258,60 @@ export class EstadoFitosanitarioComponent implements OnInit {
     } else {
       this.createChart(newData);
     }
+  }
+
+  crearPdf() {
+    const doc = new jsPDF();
+
+    const xCol1 = 15;
+    let xCol2 = 115;
+    const yLine1 = 85;
+    const lineOffset = 20;
+    let col1Offset = 60;
+    let col2Offset = 40;
+
+    doc.setFontSize(36);
+    doc.text("Estado Fitosanitario", 45, 35);
+    doc.setFontSize(24);
+    doc.text(`Lote: ${this.nombreLoteParams}`, 20, 60);
+
+    // Conteo de palmas
+    doc.setFontSize(14);
+    doc.text("Total de palmas:", xCol1, yLine1);
+    doc.text("Sanas:", xCol2, yLine1);
+    doc.text("Pendientes por tratar:", xCol1, yLine1 + lineOffset);
+    doc.text("En tratamiento:", xCol2, yLine1 + lineOffset);
+    doc.text("Pendiente por erradicar:", xCol1, yLine1 + 2 * lineOffset);
+    doc.text("Erradicadas:", xCol2, yLine1 + 2 * lineOffset);
+    doc.setFontSize(11);
+    doc.text(`${this.totalpalmas}`, xCol1 + col1Offset, yLine1);
+    doc.text(`${this.totalsanas}`, xCol2 + col2Offset, yLine1);
+    doc.text(`${this.totalpendientesportratar}`, xCol1 + col1Offset, yLine1 + lineOffset);
+    doc.text(`${this.totalentratamiento}`, xCol2 + col2Offset, yLine1 + lineOffset);
+    doc.text(`${this.totalpendientesporerradicar}`, xCol1 + col1Offset, yLine1 + 2 * lineOffset);
+    doc.text(`${this.totalerradicadas}`, xCol2 + col2Offset, yLine1 + 2 * lineOffset);
+
+    // Grafica
+    const canvas = document.getElementById("myChart") as HTMLCanvasElement;
+    doc.addImage(canvas, 'PNG', 15, 160, 180, 100);
+
+    // Conteo de enfermedades e incidencias
+    doc.addPage();
+    const yLine4 = 30;
+    xCol2 = 95;
+    col1Offset = 50;
+    col2Offset = 65;
+    doc.setFontSize(14);
+    doc.text("Casos totales:", xCol1, yLine4);
+    doc.text("Incidencia real (%):", xCol2, yLine4);
+    doc.text("Casos acumulados:", xCol1, yLine4 + lineOffset);
+    doc.text("Incidencia acumulada (%):", xCol2, yLine4 + lineOffset);
+    doc.setFontSize(11);
+    doc.text(`${this.registroEnfermedadesLote.length}`, xCol1 + col1Offset, yLine4);
+    doc.text(`${this.incidenciareal}`, xCol2 + col2Offset, yLine4);
+    doc.text(`${this.casosacumulados}`, xCol1 + col1Offset, yLine4 + lineOffset);
+    doc.text(`${this.incidenciaacumulada}`, xCol2 + col2Offset, yLine4 + lineOffset);
+
+    doc.save(`Estado_Fitosanitario-${this.nombreLoteParams}.pdf`);
   }
 }
