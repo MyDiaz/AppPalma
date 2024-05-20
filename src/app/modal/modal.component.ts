a2e3f2882958514b225c59fe2cfc8aab6fc87ce2
Jose Andres Chia Lerzundy <andreschiia1@gmail.com>
2024-05-19 20:53:43 -0500

    fix on modal component

--- src/app/modal/modal.component.ts
+++ src/app/modal/modal.component.ts
@@ -2,20 +2,20 @@
 import { Component, OnInit, Inject } from "@angular/core";
 import { MAT_DIALOG_DATA, MatDialogRef } from "@angular/material/dialog";
 import { EnfermedadesService } from "../Servicios/enfermedades.service";
-import { DomSanitizer,SafeUrl  } from '@angular/platform-browser';
+import { DomSanitizer, SafeUrl } from "@angular/platform-browser";
 import { CensosService } from "../Servicios/censos.service";
 @Component({
   selector: "app-modal",
-  templateUrl: './modal.component.html',
-  styleUrls: ['./modal.component.css']
-  })
-export class ModalComponent implements OnInit{
+  templateUrl: "./modal.component.html",
+  styleUrls: ["./modal.component.css"],
+})
+export class ModalComponent implements OnInit {
   mensaje_error: string;
   observaciones: string;
   byteArrays: any[]; // Assuming this is your array of byte arrays
   imageUrls: SafeUrl[] = [];
 
-  loading : boolean = true;
+  loading: boolean = true;
   constructor(
     @Inject(MAT_DIALOG_DATA) public data: any,
     private dialogRef: MatDialogRef<ModalComponent>,
@@ -25,32 +25,16 @@ export class ModalComponent implements OnInit{
   ) {}
 
   ngOnInit() {
-    if(this.data.rowData.id_censo !== null){
-
+    if (this.data.rowData.id_censo) {
       this.observaciones = this.data.rowData.observacion_censo;
       this.censosService.getImagenesCenso(this.data.rowData.id_censo).subscribe(
         (response) => {
-          response.forEach(r => {
-          const blob = new Blob([new Uint8Array(r.imagen.data)], { type: 'image/png' });
-          this.imageUrls.push(this.sanitizeUrl(URL.createObjectURL(blob)));
+          response.forEach((r) => {
+            const blob = new Blob([new Uint8Array(r.imagen.data)], {
+              type: "image/png",
+            });
+            this.imageUrls.push(this.sanitizeUrl(URL.createObjectURL(blob)));
           });
-          console.log('urls', this.imageUrls);
-          this.loading = false;
-        },
-        (error) => {
-          this.mensaje_error = error.error.message;
-        }
-      );
-    } else if(this.data.rowData.id_registro_enfermedad !== null ){
-
-      this.observaciones = this.data.rowData.observacion_registro_enfermedad;
-      this.enfermedadesService.getImagenesRegistroEnfermedad(this.data.rowData.id_registro_enfermedad).subscribe(
-        (response) => {
-          response.forEach(r => {
-          const blob = new Blob([new Uint8Array(r.imagen.data)], { type: 'image/png' });
-          this.imageUrls.push(this.sanitizeUrl(URL.createObjectURL(blob)));
-          });
-          console.log('urls', this.imageUrls);
           this.loading = false;
         },
         (error) => {
@@ -58,7 +42,26 @@ export class ModalComponent implements OnInit{
         }
       );
     }
-
+    if (this.data.rowData.id_registro_enfermedad) {
+      this.observaciones = this.data.rowData.observacion_registro_enfermedad;
+      this.enfermedadesService
+        .getImagenesRegistroEnfermedad(this.data.rowData.id_registro_enfermedad)
+        .subscribe(
+          (response) => {
+            response.forEach((r) => {
+              const blob = new Blob([new Uint8Array(r.imagen.data)], {
+                type: "image/png",
+              });
+              this.imageUrls.push(this.sanitizeUrl(URL.createObjectURL(blob)));
+            });
+            console.log("urls", this.imageUrls);
+            this.loading = false;
+          },
+          (error) => {
+            this.mensaje_error = error.error.message;
+          }
+        );
+    }
   }
 
   sanitizeUrl(url: string) {
