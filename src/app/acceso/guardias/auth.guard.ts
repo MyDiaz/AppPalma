1e0d0d15ff227e97a5cdff5b58a535e16363411f
Andres Chia <andreschiia1@gmail.com>
2025-02-18 23:05:58 -0500

    changes for production

--- src/app/acceso/guardias/auth.guard.ts
+++ src/app/acceso/guardias/auth.guard.ts
@@ -9,17 +9,17 @@ import { Router } from '@angular/router'
 })
 export class AuthGuard implements CanActivate {
 
-  constructor( private auth_usuario:AuthService, private router:Router  ){}
+  constructor(private auth_usuario: AuthService, private router: Router) { }
 
   canActivate(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-      if ( this.auth_usuario.isLoggedIn ){
-        return true;
-      }else{
-        this.router.navigate(["/login"]);
-        return false;
-         
-      }  
+    if (this.auth_usuario.isLoggedIn) {
+      return true;
+    } else {
+      this.router.navigate(["login"]);
+      return false;
+
+    }
   }
 }
