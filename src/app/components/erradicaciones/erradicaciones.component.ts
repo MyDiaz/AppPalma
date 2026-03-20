8f7390a5bfb3dca727720fecb54f5de18b739b9b
Mayra Riascos Diaz <diazmaya947@ymail.com>
2026-03-20 17:10:23 -0300

    crear componente

---
+++ src/app/components/erradicaciones/erradicaciones.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "app-erradicaciones",
+  templateUrl: "./erradicaciones.component.html",
+  styleUrls: ["./erradicaciones.component.css"],
+})
+export class ErradicacionesComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
