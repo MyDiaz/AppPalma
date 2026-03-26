fe1386e773a9393a42437e2cc9103552ed999008
Mayra Riascos Diaz <diazmaya947@ymail.com>
2026-03-26 20:17:38 -0300

    creación del componente

---
+++ src/app/components/fertilizaciones/fertilizaciones.component.ts
@@ -0,0 +1,8 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "app-fertilizaciones",
+  templateUrl: "./fertilizaciones.component.html",
+  styleUrls: ["./fertilizaciones.component.css"],
+})
+export class FertilizacionesComponent {}
